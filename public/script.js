2385685d74280c73eae5d9c7e0cbd5d20329c731
Waltzoor <valttiw@gmail.com>
2025-02-12 17:02:32 +0200

    game-board css

--- public/script.js
+++ public/script.js
@@ -1,6 +1,8 @@
 
-document.getElementById('button').addEventListener('click',startGame);
+document.getElementById('new-game-btn').addEventListener('click',startGame);
 
 function startGame(){
     console.log('klikattu');
+    document.getElementById('intro-screen').style.display = 'none';
+    document.getElementById('game-screen').style.display = 'block';
 }

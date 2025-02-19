bcc025cc563900ad0c3f901ef922d06e71e20e16
Waltzoor <valttiw@gmail.com>
2025-02-19 16:57:35 +0200

    pelilauta luotu

--- public/script.js
+++ public/script.js
@@ -1,8 +1,27 @@
+const BOARD_SIZE = 15;
+let board;
 
 document.getElementById('new-game-btn').addEventListener('click',startGame);
 
 function startGame(){
-    console.log('klikattu');
+    console.log('Clik');
     document.getElementById('intro-screen').style.display = 'none';
     document.getElementById('game-screen').style.display = 'block';
+ 
+    board = generateRandomBoard();
+}
+
+function generateRandomBoard(){
+    const newBoard = Array.from({length:BOARD_SIZE},()=>Array(BOARD_SIZE).fill(''))
+
+    for(let y = 0; y < BOARD_SIZE; y++){
+        for(let x = 0; x < BOARD_SIZE; x++){  
+            if(y === 0){
+                newBoard[y][x] = 'W';
+        }
+    }
+}
+
+    console.log(newBoard);
+    return newBoard;
 }
